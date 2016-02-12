2b32e8de5f4e222898c5513c15fd1fce7770e66e
pontikos <npontikos@gmail.com>
2016-02-12 20:39:32 +0000

    parameters.sh

--- parameters.sh
+++ parameters.sh
@@ -1,17 +1,11 @@
 # for the user to set
+chrs="1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22"
 #famFile=exome-pedigree.fam
 famFile=jel.fam
-modelFile=/home/rejudcu/exomeLinkage/dominant.model
-vcfPrefix=../chr
 # e.g. /home/rejudcu/exomeLinkage/exomes/chr13.vcf.gz
+vcfPrefix=../chr
 
-#chrs="1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22"
-chrs="22"
-# chrs="1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22"
-#chrs="22 21 20 19 18 17 16 15 14 13 12 11 10 9 8 7 6 5 4 3 2 1 "
-# small ones first to check it works OK
-# chrs="9"
-
+modelFile=/home/rejudcu/exomeLinkage/dominant.model
 MEGA2=/home/rejudcu/mega2/mega2_v4.8.2_src/srcdir/mega2_linux
 MAKEFAKEMAPBIN=/home/rejudcu/exomeLinkage/makeFakeMap
 MERLINFOLDER=/cluster/project8/vyp/AdamLevine/software/merlin
